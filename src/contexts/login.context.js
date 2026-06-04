cf4bad8a452e581f66ea1d8d1abc3908ca5fcbe6
tarsem singh <Tarsem.Singh@amlgolabs.com>
2026-06-04 20:44:12 +0530

    adding logout prod code

--- src/contexts/login.context.js
+++ src/contexts/login.context.js
@@ -322,7 +322,7 @@ export const AuthProvider = ({ children }) => {
     const logoutUser = async () => {
         try {
             const res = await axios.get(
-            "http://localhost:5001/api/v1/students/logout",
+            `${process.env.NEXT_PUBLIC_ENVIRONMENT=='Development' ? process.env.NEXT_PUBLIC_BASE_URL_LOCAL : process.env.NEXT_PUBLIC_BASE_URL_PROD }/api/v1/students/logout`,
             {
                 withCredentials: true,
             }
