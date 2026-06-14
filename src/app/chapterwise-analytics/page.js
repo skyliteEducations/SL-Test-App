2d2318aac638d32218096339411af8ba40ea9bf5
tarsem singh <Tarsem.Singh@amlgolabs.com>
2026-06-14 13:39:10 +0530

    fixing 2.0

--- src/app/chapterwise-analytics/page.js
+++ src/app/chapterwise-analytics/page.js
@@ -1,15 +1,47 @@
+// 'use client'
+// import Landing from './landing'
+// import { useSearchParams } from "next/navigation";
+// export default function ResultPage(){
+//     const searchParams = useSearchParams();
+
+//     const subject = searchParams.get("subject");
+//     const sheetId = searchParams.get("sheet");
+//     const chapterName = searchParams.get("chapterName");
+
+
+//     return(
+//         <Landing subject={subject} sheetId={sheetId} chapterName={chapterName} />
+//     )
+// }
+
 'use client'
+
+import { useEffect, useState } from 'react'
 import Landing from './landing'
-import { useSearchParams } from "next/navigation";
-export default function ResultPage(){
-    const searchParams = useSearchParams();
 
-    const subject = searchParams.get("subject");
-    const sheetId = searchParams.get("sheet");
-    const chapterName = searchParams.get("chapterName");
+export default function ResultPage() {
 
+    const [params, setParams] = useState({
+        subject: '',
+        sheetId: '',
+        chapterName: ''
+    })
 
-    return(
-        <Landing subject={subject} sheetId={sheetId} chapterName={chapterName} />
+    useEffect(() => {
+        const searchParams = new URLSearchParams(window.location.search)
+
+        setParams({
+            subject: searchParams.get("subject"),
+            sheetId: searchParams.get("sheet"),
+            chapterName: searchParams.get("chapterName")
+        })
+    }, [])
+
+    return (
+        <Landing
+            subject={params.subject}
+            sheetId={params.sheetId}
+            chapterName={params.chapterName}
+        />
     )
 }
