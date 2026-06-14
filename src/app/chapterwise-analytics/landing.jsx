5a739070b23a6090d4eeaaec54141bb4c13b4978
tarsem singh <Tarsem.Singh@amlgolabs.com>
2026-06-14 13:28:44 +0530

    fixing server mess

--- src/app/chapterwise-analytics/landing.jsx
+++ src/app/chapterwise-analytics/landing.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useContext, useEffect } from 'react'
 import { ChapterSubmitContext } from '@/contexts/chapterwiseSubmission.context'
 import FullScreenLoader from '../components/fullLoader'
